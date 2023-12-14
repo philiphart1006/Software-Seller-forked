000ffb6a04d38bd3117cf539453c97108dc5837f
philiphart1006 <philiphart1006@gmail.com>
2023-12-14 16:02:56 +0000

    create heroku app and set config

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 import router from './config/routes.js'
 
+import 'dotenv/config' // only needs to be added if it doesn't already exist
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+
 const app = express()
 
 app.use(express.json())
@@ -13,6 +22,12 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', router)
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 async function startServer(){
   try {
